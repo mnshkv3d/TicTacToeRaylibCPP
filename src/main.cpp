4ceaa6e183f6f1c22f9dcc24c294209681ddcc50
mnshkv <aleksandr.menshikov@gmail.com>
2024-07-11 17:30:29 +0500

    modofiying game loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <cstdio>
 #include <vector>
 #include "include/raylib-cpp.hpp"
@@ -12,9 +13,21 @@ const int screenWidth = 1280;
 const int screenHeight = 800;
 
 enum CellValue { EMPTY, X, O };
-enum GameState { PLAYER1, PLAYER2, END };
+enum GameMode { HOTSEAT, VSPC };
+enum GameState { MAINMENU, PLAYER_X_MOVE, PLAYER_O_MOVE, END };
 
-struct Cell {
+struct GameManager
+{
+    GameMode CurrentGameMode;
+    GameState CurrentGameState;
+
+    GameManager(GameMode CurrentGameMode = HOTSEAT, GameState CurrentGameState = MAINMENU)
+        : CurrentGameMode(CurrentGameMode), CurrentGameState(CurrentGameState)
+    {}
+};
+
+struct Cell
+{
     int indexI;
     int indexJ;
     CellValue value;
@@ -28,7 +41,7 @@ public:
     Grid();
     void GridInit();
     void DrawGrid();
-    void ChangeCellState(Vector2 ballPosition);
+    void ChangeCellState(Vector2 MousePosition, GameManager& GameManager);
 
     Cell& getCell(int i, int j);
 
@@ -36,25 +49,46 @@ private:
     std::vector<std::vector<Cell>> grid;
 };
 
+class Player
+{
+public:
+};
+
 int main()
 {
 
     raylib::Window window(screenWidth, screenHeight, "PingTacPong");
     window.SetTargetFPS(60);
+    GameManager gm;
     Grid grid;
-
+    gm.CurrentGameState = PLAYER_X_MOVE;
     while (!WindowShouldClose())
     {
         // Game loop update section
-        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && gm.CurrentGameState != MAINMENU)
         {
-            grid.ChangeCellState(GetMousePosition());
+            if (gm.CurrentGameState == PLAYER_X_MOVE)
+            {
+                grid.ChangeCellState(GetMousePosition(), gm);
+                printf("Player X move\n");
+                gm.CurrentGameState = PLAYER_O_MOVE;
+            }
+            else if (gm.CurrentGameState == PLAYER_O_MOVE)
+            {
+                grid.ChangeCellState(GetMousePosition(), gm);
+                printf("Player O move \n");
+                gm.CurrentGameState = PLAYER_X_MOVE;
+            }
         }
 
         // Drawing section
         BeginDrawing();
-        window.ClearBackground(BLACK);
-        grid.DrawGrid();
+        window.ClearBackground(RAYWHITE);
+        if (gm.CurrentGameState != MAINMENU)
+        {
+            grid.DrawGrid();
+        }
+
         EndDrawing();
     }
 
@@ -89,7 +123,7 @@ void Grid::DrawGrid()
             int y = ((screenHeight / 2) - 300) + j * cellHeight;
 
             DrawRectangle(x, y, cellWidth, cellHeight, GREEN);
-            DrawRectangleLines(x, y, cellWidth, cellHeight, WHITE);
+            DrawRectangleLines(x, y, cellWidth, cellHeight, RAYWHITE);
 
             // Drawing cell contents based on their value
             if (grid[i][j].value == X)
@@ -105,26 +139,22 @@ void Grid::DrawGrid()
     }
 }
 
-void Grid::ChangeCellState(Vector2 MousePosition)
+void Grid::ChangeCellState(Vector2 MousePosition, GameManager& GameManager)
 {
     // check if mouse in Cells area
     if (MousePosition.x >= (screenWidth / 2) - 300 && MousePosition.x <= (screenWidth / 2) + 300 &&
         MousePosition.y >= (screenHeight / 2) - 300 && MousePosition.y <= (screenHeight / 2) + 300)
     {
-
         int i = (MousePosition.x - ((screenWidth / 2) - 300)) / cellWidth;
         int j = (MousePosition.y - ((screenHeight / 2) - 300)) / cellHeight;
-
         // check index for valid
         if (i >= 0 && i < COLS && j >= 0 && j < ROWS)
         {
             // change cell state
-            if (grid[i][j].value == EMPTY)
+            if (grid[i][j].value == EMPTY && GameManager.CurrentGameState == PLAYER_X_MOVE)
                 grid[i][j].value = X;
-            else if (grid[i][j].value == X)
+            else if (grid[i][j].value == EMPTY && GameManager.CurrentGameState == PLAYER_O_MOVE)
                 grid[i][j].value = O;
-            else if (grid[i][j].value == O)
-                grid[i][j].value = EMPTY;
         }
     }
 }
@@ -133,3 +163,34 @@ Cell& Grid::getCell(int i, int j)
 {
     return grid[i][j];
 }
+
+// // check index for valid
+// if (i >= 0 && i < COLS && j >= 0 && j < ROWS)
+// {
+//     // change cell state
+//     if (grid[i][j].value == EMPTY)
+//         grid[i][j].value = X;
+//     else if (grid[i][j].value == X)
+//         grid[i][j].value = O;
+//     else if (grid[i][j].value == O)
+//         grid[i][j].value = EMPTY;
+// }
+
+// if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT))
+// {
+//     for (int i = 0; i < COLS; ++i)
+//     {
+//         for (int j = 0; j < ROWS; ++j)
+//         {
+//             Cell& cell = grid.getCell(i, j);
+//             std::cout << "Cell (" << cell.indexI << ", " << cell.indexJ << "): ";
+//             switch (cell.value)
+//             {
+//                 case EMPTY: std::cout << "EMPTY"; break;
+//                 case X: std::cout << "X"; break;
+//                 case O: std::cout << "O"; break;
+//             }
+//             std::cout << std::endl;
+//         }
+//     }
+// }
